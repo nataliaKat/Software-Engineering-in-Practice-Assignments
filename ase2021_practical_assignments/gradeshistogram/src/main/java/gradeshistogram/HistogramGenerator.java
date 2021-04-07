664282be2b93546098299ee2d1c1ffd7458ddad2
Natalia Katsiapi <kat26natalia@gmail.com>
2021-04-08 01:16:54 +0300

    Change diagram to histogram

--- ase2021_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ ase2021_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,6 +12,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
@@ -23,7 +25,7 @@ import org.jfree.data.xy.XYSeriesCollection;
 public class HistogramGenerator {
 
 	/**
-	 * Reads the grades from the file and returns them as an array of doubles. 
+	 * Reads the grades from the file and returns them as an array of doubles.
 	 * 
 	 * @param filename
 	 * @return gradesAsArray A double array of grades.
@@ -31,7 +33,7 @@ public class HistogramGenerator {
 	public double[] getGrades(String filename) {
 		List<Double> grades = new ArrayList();
 		File gradesFile = new File(filename);
-		Scanner reader;	
+		Scanner reader;
 		try {
 			reader = new Scanner(gradesFile);
 			while (reader.hasNextLine()) {
@@ -53,57 +55,29 @@ public class HistogramGenerator {
 		}
 		return gradesAsArray;
 	}
-	
+
 	/**
-	 * Receives a single dimension Integer array. From this array the dataset
-	 * that will be used for the visualization is generated. Finally, The chart
-	 * is generated with the use of the aforementioned dataset and then
-	 * presented in the screen.
+	 * Receives a single dimension array of ints. From this array the dataset that
+	 * will be used for the visualization is generated. Finally, The chart is
+	 * generated and then presented in the screen.
 	 * 
 	 * @param dataValues Single dimension double array
 	 */
 	public void generateChart(double[] dataValues) {
-		/*
-		 * The XYSeriesCollection object is a set XYSeries series (dataset) that
-		 * can be visualized in the same chart
-		 */
-		XYSeriesCollection dataset = new XYSeriesCollection();
-		/*
-		 * The XYSeries that are loaded in the dataset. There might be many
-		 * series in one dataset.
-		 */
-		XYSeries data = new XYSeries("random values");
 
-		/*
-		 * Populating the XYSeries data object from the input Integer array
-		 * values.
-		 */
-		for (int i = 0; i < dataValues.length; i++) {
-			data.add(i, dataValues[i]);
-		}
+		HistogramDataset dataset = new HistogramDataset();
+		
+		// This calculates the number of bins to be used which is the maximum grade + 1
+		int numberOfBins = (int) Arrays.stream(dataValues).max().orElse(-1) + 1;
+		dataset.addSeries("key", dataValues, numberOfBins);
 
-		// add the series to the dataset
-		dataset.addSeries(data);
-
-		boolean legend = false; // do not visualize a legend	
-		boolean tooltips = false; // do not visualize tooltips
-		boolean urls = false; // do not visualize urls
-
-		// Declare and initialize a createXYLineChart JFreeChart
-		JFreeChart chart = ChartFactory.createXYLineChart("Grades Histogram", "Students", "Grades", dataset,
-				PlotOrientation.VERTICAL, legend, tooltips, urls);
-
-		/*
-		 * Initialize a frame for visualizing the chart and attach the
-		 * previously created chart.
-		 */
-		ChartFrame frame = new ChartFrame("Grades", chart);
+		JFreeChart histogram = ChartFactory.createHistogram("Grades Histogram", "Grade", "Frequency", dataset);
+		ChartFrame frame = new ChartFrame("Grades", histogram);
 		frame.pack();
 		// makes the previously created frame visible
 		frame.setVisible(true);
 	}
 
-	
 	public static void main(String[] args) {
 		HistogramGenerator hs = new HistogramGenerator();
 		double[] grades = hs.getGrades(args[0]);
