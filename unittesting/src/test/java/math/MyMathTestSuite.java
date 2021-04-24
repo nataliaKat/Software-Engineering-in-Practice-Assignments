1ba02cffc4834a11c460412b2598cb2555a575c6
Natalia Katsiapi <kat26natalia@gmail.com>
2021-04-25 02:36:26 +0300

    Add doc in test suite

--- unittesting/src/test/java/math/MyMathTestSuite.java
+++ unittesting/src/test/java/math/MyMathTestSuite.java
@@ -3,6 +3,11 @@ package math;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * Groups MyMathTest and MyMathFactorialParameterizedTest.
+ * 
+ * @author Natalia Katsiapi
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({MyMathTest.class, MyMathFactorialParameterizedTest.class})
 public class MyMathTestSuite {
