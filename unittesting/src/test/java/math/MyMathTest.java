25552d3ae2e473b3353903edc70d15a528fb35fc
Natalia Katsiapi <kat26natalia@gmail.com>
2021-04-25 02:29:07 +0300

    Add tests for exceptions for factorial

---
+++ unittesting/src/test/java/math/MyMathTest.java
@@ -0,0 +1,39 @@
+package math;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * MyMathTest provides testing methods for MyMath class.
+ *
+ * @author Natalia Katsiapi
+ */
+public class MyMathTest {
+
+    MyMath myMath = new MyMath();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    /**
+     * Tests factorial() for negative values.
+     */
+    @Test
+    public void testFactorialMinusOne() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("n should be >= 0 and < 12");
+        myMath.factorial(-1);
+    }
+
+    /**
+     * Tests factorial for > 12 values.
+     */
+    @Test
+    public void testFactorialThirteen() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("n should be >= 0 and < 12");
+        myMath.factorial(13);
+    }
+
+}
